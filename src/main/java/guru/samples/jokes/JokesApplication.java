3c631914718bc2c84efef6797287b452ba9bb745
digid0c <digidoc22@gmail.com>
2020-01-21 10:56:12 +0200

    Replace Java based config with XML based config

--- src/main/java/guru/samples/jokes/JokesApplication.java
+++ src/main/java/guru/samples/jokes/JokesApplication.java
@@ -2,8 +2,10 @@ package guru.samples.jokes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:chuck-norris-configuration.xml")
 public class JokesApplication {
 
     public static void main(String[] args) {
